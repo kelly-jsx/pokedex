f92d762cf65e0edf083f60a004dcb083d01a01ea
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-01-29 13:26:44 +0100

    refactor App comtonent to class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,59 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 
 import GitHub from "./components/GitHub";
 import Header from "./components/Header/Header";
 import PokeCard from "./components/PokeCard";
 
-function App() {
-  const [pokemon, setPokemon] = useState({});
-  axios
-    .get("https://pokeapi.co/api/v2/pokemon/bulbasaur")
-    .then(function (response) {
-      const data = response.data;
-      setPokemon({
-        index: data.id,
-        name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
-        imgUrl: data.sprites.other.dream_world.front_default,
-      });
-    });
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allPokemons: [],
+      pokemon: {
+        index: "",
+        name: "",
+        imgUrl: "",
+      },
+    };
+  }
 
-  return (
-    <div className="App m-2">
-      <GitHub />
-      <Header />
-      <div className="poke-list flex mt-2 bg-slate-600">
-        <PokeCard
-          key={pokemon.index}
-          index={pokemon.index}
-          name={pokemon.name}
-          imgUrl={pokemon.imgUrl}
-        />
+  componentDidMount() {
+    this.getPokemonInfo();
+  }
+
+  getPokemonInfo = async () => {
+    await axios
+      .get("https://pokeapi.co/api/v2/pokemon/bulbasaur")
+      .then((response) => {
+        const data = response.data;
+        console.log(response.data);
+        this.setState({
+          pokemon: {
+            index: data.id,
+            name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
+            imgUrl: data.sprites.other.dream_world.front_default,
+          },
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="App m-2">
+        <GitHub />
+        <Header />
+        <div className="poke-list flex mt-2 bg-slate-600">
+          <PokeCard
+            key={this.state.pokemon.index}
+            index={this.state.pokemon.index}
+            name={this.state.pokemon.name}
+            imgUrl={this.state.pokemon.imgUrl}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 const PokeCard = (pokemon) => {
   return (
-    <div>
-      <div className="poke-card text-center">
-        <p>#{pokemon.index}</p>
-        <img src={pokemon.imgUrl} alt="" />
-        <p>{pokemon.name}</p>
-      </div>
+    <div className="poke-card text-center">
+      <p>#{pokemon.index}</p>
+      <img src={pokemon.imgUrl} alt="" />
+      <p>{pokemon.name}</p>
     </div>
   );
 };
