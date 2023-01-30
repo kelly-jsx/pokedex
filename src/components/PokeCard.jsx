afa1b72033f14184b288df01273906d0265e685d
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-01-30 17:22:56 +0100

    styling PokeCard component

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -21,15 +21,15 @@ const PokeCard = ({ id, image, name, type }) => {
     // >
     <div
       className={`poke-card cursor-pointer text-center bg-poke-blue rounded-lg p-3
-       transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-103 hover:bg-poke-yellow duration-1
+       transition ease-in-out delay-150 hover:-translate-y-1.5 hover:scale-103 hover:bg-poke-yellow duration-1
         `}
     >
-      <p>#{String(id).padStart(3, "0")}</p>
-      <img className={"mx-auto h-60"} src={image} alt="" />
-      <p className="capitalize font-bold">{name}</p>
-      <div className="flex justify-center">
+      <p className={"text-2xl"}>#{String(id).padStart(3, "0")}</p>
+      <img className={"mx-auto h-60 mt-5"} src={image} alt="" />
+      <p className="capitalize font-bold mt-5 text-xl">{name}</p>
+      <div className="flex justify-center mt-5">
         {type.map((type) => (
-          <div className={`rounded-full p-2 m-2 mt-5 bg-${type.type.name}`}>
+          <div className={`rounded-full p-2 m-2 bg-${type.type.name}`}>
             {/*<p>{type.type.name}</p>*/}
             <img
               className={"h-8"}
